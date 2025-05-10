f593679c501f75e3bb29af16e28c42b349a4fce8
Casserdic <casserdic@gmail.com>
2025-05-10 08:38:11 +0700

    testing pushes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,9 @@
         <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
         <h1 class="text-h2 font-weight-bold">Vuetify</h1>
       </div>
-
+      <v-row>
+        <v-col>testing pushes github</v-col>
+      </v-row>
       <v-row>
         <v-col cols="12">
           <v-card
